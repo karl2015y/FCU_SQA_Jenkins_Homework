d706853320fd7d3d7665eb8d4a2c686eb8c87c59
karl2015y <karl2015y@gmail.com>
2021-05-29 08:03:29 +0800

    解開測試註解

--- app/src/test/java/main/DiscountTest.java
+++ app/src/test/java/main/DiscountTest.java
@@ -9,12 +9,12 @@ public class DiscountTest {
 	class DifferentAges {
 		String dateTime = "2021-05-26 週三 14:30:00";
 
-		// @Test
-		// public void testAgeHasDiscount() throws Throwable {
-		// 	Identity identity = new Identity(10, false, false);
-		// 	Discount discount = new Discount(identity, dateTime);
-		// 	Assertions.assertEquals(0.8, discount.getDiscount());
-		// }
+		@Test
+		public void testAgeHasDiscount() throws Throwable {
+			Identity identity = new Identity(10, false, false);
+			Discount discount = new Discount(identity, dateTime);
+			Assertions.assertEquals(0.8, discount.getDiscount());
+		}
 
 		@Test
 		public void testAgeLessThan() throws Throwable {
@@ -30,12 +30,12 @@ public class DiscountTest {
 	@DisplayName("營業與非營業時間")
 	@Nested
 	class DifferentBusinessHours {
-		// @Test
-		// public void testEarlyBirdBusiness() throws Throwable {
-		// 	Identity identity = new Identity(25, false, false);
-		// 	Discount discount = new Discount(identity, "2021-05-26 週三 06:30:00");
-		// 	Assertions.assertEquals(0.8, discount.getDiscount());
-		// }
+		@Test
+		public void testEarlyBirdBusiness() throws Throwable {
+			Identity identity = new Identity(25, false, false);
+			Discount discount = new Discount(identity, "2021-05-26 週三 06:30:00");
+			Assertions.assertEquals(0.8, discount.getDiscount());
+		}
 
 		@Test
 		public void testHaveBusiness() throws Throwable {
